75a1d17899eaf776f3a887471af5eb6ee1c617a5
Bruno Senigalha <brsenigalha@gmail.com>
2023-12-04 09:32:16 -0300

    SimpleSearch

---
+++ SalesWebMvc/Models/Services/SalesRecordService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SalesWebMvc.Data;
+
+namespace SalesWebMvc.Models.Services
+{
+    public class SalesRecordService(SalesWebMvcContext context)
+    {
+        private readonly SalesWebMvcContext _context = context;
+
+        public async Task<List<SalesRecord>> FindByDateAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.SalesRecords select obj;
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= minDate.Value);
+            }
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Date <= maxDate.Value);
+            }
+            return await result
+                .Include(x => x.Seller)
+                .Include(x => x.Seller.Department)
+                .OrderByDescending(x => x.Date)
+                .ToListAsync();
+        }
+
+    }
+
+
+}
